679b816597df87f84e48af1f42f31a6917adbb80
jfqd <jfqd@blun.org>
2013-03-23 18:04:54 +0100

    precompile assest

--- config/application.rb
+++ config/application.rb
@@ -71,6 +71,6 @@ module Zlist
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.0.2'
   end
 end
