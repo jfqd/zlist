fc7d9fbcac1d7b18958bec68ecc315ee3716ac31
jfqd <jfqd@blun.org>
2013-03-25 12:05:27 +0100

    add documentation to methods

--- app/models/mail_handler.rb
+++ app/models/mail_handler.rb
@@ -7,40 +7,50 @@ class MailHandler < ActionMailer::Base
   end
   
   # Processes incoming emails
-  # Returns true or false
+  # Returns true or false on any errors
   def receive(email)
     @email = email
-    return false if !subscriber? && !subscribe_request?
+    return false if not subscriber? and not subscribe_request?
     Inbound::Email.new(hash).process
     return true
   end
   
   private
   
+  # Regex to extract the topic.id out of the subject
   TOPIC_RE = %r{\[[^\]]*#(\d+)\]}
   
+  # Returns from which mail-address the email was send
   def from
     @from ||= @email.from.to_a.first.to_s.strip
   end
   
+  # Returns the mail-address of the mailinglist. For
+  # subscribe requests it is the first mail-address
+  # in the array
   def to
-    @to
+    @to || @email.to.first
   end
   
+  # Returns carbon copy-addresses as a comma separated string
   def cc
-    @email.cc.nil?  ? '' : @email.cc.join(', ')
+    @email.cc.nil? ? '' : @email.cc.join(', ')
   end
   
+  # Returns the subject of the email
   def subject
     @subject ||= @email.subject.to_s
   end
   
+  # Returns the subscribers of the requested mailinglist
   def subscriber
     @subscriber ||= (from.present? ? Subscriber.find_by_email(from) : nil)
   rescue
     nil
   end
   
+  # Returns true if the sender is a subscriber of
+  # the requested mailinglist
   def subscriber?
     if subscriber.nil?
       log "MailHandler: ignoring email from unknown user [#{from}]"
@@ -69,28 +79,43 @@ class MailHandler < ActionMailer::Base
     return true
   end
   
+  # Returns true if it is a subscribe request
   def subscribe_request?
     subject.downcase == "subscribe"
   end
   
+  # Returns the text-part of the email. Falling
+  # back to the email itself if it is plain-text
   def text_part
-    @text_part ||= @email.html_part || @email
+    @text_part ||= @email.text_part || @email
   end
   
+  # Returns the html-part of the email if present.
+  # If not nil is returned
   def html_part
     @html_part ||= @email.html_part
   end
   
+  # Returns utf8 encoded parts
   def encode(part)
     Zlist::Encoder.to_utf8(part.body.decoded, part.charset)
   end
   
+  # Returns an array of the email-headers
   def header
     @header ||= (@email.header.nil? || @email.header.to_s.nil? ? [] : @email.header.to_s.split("\r\n"))
   rescue
     []
   end
   
+  # Name of the mailinglist, for subscribe
+  # requests it is the name of the account
+  # without the domain
+  def mailbox
+    @mailbox || to.split('@')[0]
+  end
+  
+  # Returns the topic.id, present only for replies
   def mailbox_hash
     m = subject.match(TOPIC_RE)
     m[1] || ''
@@ -98,6 +123,8 @@ class MailHandler < ActionMailer::Base
     ''
   end
   
+  # Returns a hash with a relevant information
+  # for the next step in process
   def hash
     hash = {}
     hash[:subject]      = subject
@@ -110,15 +137,17 @@ class MailHandler < ActionMailer::Base
     hash[:reply]        = @email.reply_to
     hash[:message_id]   = @email.message_id
     hash[:mailbox_hash] = mailbox_hash
-    hash[:mailbox]      = @mailbox
+    hash[:mailbox]      = mailbox
     # puts hash.inspect
     hash
   end
   
+  # Returns true if the email has attachements
   def attachements?
     @email.has_attachments?
   end
   
+  # log a message to Rails.logger if present
   def log(message)
     logger.info(message) if logger && logger.info
   end
